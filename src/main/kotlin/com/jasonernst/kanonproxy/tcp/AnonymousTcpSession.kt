45293ce0962411a4e40ea73e2915e4651afc37df
Jason Ernst <ernstjason1@gmail.com>
2024-10-18 13:22:36 -0700

    lint fix

--- src/main/kotlin/com/jasonernst/kanonproxy/tcp/AnonymousTcpSession.kt
+++ src/main/kotlin/com/jasonernst/kanonproxy/tcp/AnonymousTcpSession.kt
@@ -5,10 +5,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
-import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.InetSocketAddress
-import java.net.StandardProtocolFamily
 import java.nio.channels.SocketChannel
 import java.util.concurrent.LinkedBlockingDeque
 
