c9cf30e160a97f02484b4b4f49f2b33f90d6008e
Jason Ernst <ernstjason1@gmail.com>
2024-12-02 15:49:05 -0800

    change mutex -> mutable state flow

--- core/src/main/kotlin/com/jasonernst/kanonproxy/BidirectionalByteChannel.kt
+++ core/src/main/kotlin/com/jasonernst/kanonproxy/BidirectionalByteChannel.kt
@@ -3,8 +3,6 @@ package com.jasonernst.kanonproxy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import java.nio.ByteBuffer
 import java.nio.channels.ByteChannel
 import kotlin.math.min
@@ -12,8 +10,8 @@ import kotlin.math.min
 class BidirectionalByteChannel : ByteChannel {
     private val buffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
     private var isOpen = true
+    private val readyToWrite = MutableStateFlow(true)
     private val readyToRead = MutableStateFlow(false)
-    private val mutex = Mutex()
 
     override fun isOpen(): Boolean = isOpen
 
@@ -23,43 +21,49 @@ class BidirectionalByteChannel : ByteChannel {
     }
 
     override fun write(src: ByteBuffer): Int {
-        return runBlocking {
-            mutex.withLock {
-                val availableBytes = min(buffer.remaining(), src.remaining())
-                buffer.put(src.array(), src.position(), availableBytes)
-                src.position(src.position() + availableBytes)
-                readyToRead.value = true
-                return@runBlocking availableBytes
+        if (readyToWrite.value.not()) {
+            runBlocking {
+                readyToWrite.takeWhile { !it }.collect {}
             }
         }
+        readyToRead.value = false
+        val availableBytes = min(buffer.remaining(), src.remaining())
+        buffer.put(src.array(), src.position(), availableBytes)
+        src.position(src.position() + availableBytes)
+        readyToRead.value = true
+        return availableBytes
     }
 
     override fun read(dst: ByteBuffer): Int {
-        return runBlocking {
-            mutex.withLock {
-                // when this function is called, we expect the buffer is pointing to the end of what was written to it
-                // if its at zero, there is nothing to read
-                if (buffer.position() == 0) {
-                    runBlocking {
-                        readyToRead.takeWhile { !it }.collect {}
-                    }
-                }
-                if (!isOpen) {
-                    return@runBlocking 0
-                }
-                // flip the buffer to get it from write mode to read mode
-                buffer.flip()
-                val availableBytes = min(buffer.remaining(), dst.remaining())
-                dst.put(buffer.array(), buffer.position(), availableBytes)
-                buffer.position(buffer.position() + availableBytes)
-                if (!buffer.hasRemaining()) {
-                    readyToRead.value = false
-                }
-                // compact to get us back into read mode
-                buffer.compact()
-                return@runBlocking availableBytes
+        // when this function is called, we expect the buffer is pointing to the end of what was written to it
+        // if its at zero, there is nothing to read
+        if (buffer.position() == 0) {
+            runBlocking {
+                readyToRead.takeWhile { !it }.collect {}
             }
         }
+        if (!isOpen) {
+            return 0
+        }
+        readyToWrite.value = false
+        // just in case its flipped in the time it took us to get here
+        if (readyToRead.value.not()) {
+            readyToWrite.value = true
+            return 0
+        }
+
+        // flip the buffer to get it from write mode to read mode
+        buffer.flip()
+        val availableBytes = min(buffer.remaining(), dst.remaining())
+        dst.put(buffer.array(), buffer.position(), availableBytes)
+        buffer.position(buffer.position() + availableBytes)
+        if (!buffer.hasRemaining()) {
+            readyToRead.value = false
+        }
+        // compact to get us back into read mode
+        buffer.compact()
+        readyToWrite.value = true
+        return availableBytes
     }
 
     fun available(): Int =
