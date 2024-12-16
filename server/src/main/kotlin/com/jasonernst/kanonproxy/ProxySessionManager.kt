c3cb49fcb173319298cb9cb3c9b21aa7e2d68cf2
Jason Ernst <ernstjason1@gmail.com>
2024-12-16 15:31:06 -0800

    Switch server to a non-blocking datagram channel

--- server/src/main/kotlin/com/jasonernst/kanonproxy/ProxySessionManager.kt
+++ server/src/main/kotlin/com/jasonernst/kanonproxy/ProxySessionManager.kt
@@ -1,7 +1,13 @@
 package com.jasonernst.kanonproxy
 
 import java.net.InetSocketAddress
+import java.nio.ByteBuffer
 
 interface ProxySessionManager {
+    fun enqueueOutgoing(
+        clientAddress: InetSocketAddress,
+        buffer: ByteBuffer,
+    )
+
     fun removeSession(clientAddress: InetSocketAddress)
 }
