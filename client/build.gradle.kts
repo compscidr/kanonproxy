4d08796c6ee21d4ba7061af6af915a97d0b37606
Jason Ernst <ernstjason1@gmail.com>
2024-12-18 10:04:23 -0800

    Update to jdk 21

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -21,12 +21,12 @@ tasks.withType<Test>().configureEach {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 jacoco {
